5ab2210d27bc34a78d3f2bacd99f5e68ccc25f59
bilal <bilalghazal98@gmail.com>
2025-08-20 13:59:51 +0300

    Set up the page for adding games to the db and style it

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,12 +4,12 @@ const db = require("../db/queries")
 async function getHomepage(req, res) {
     const data = await db.getGamesInfo()
 
-    res.render("index", {title: "Homepage", data: data})
+    res.render("pages/index", {title: "Homepage", data: data})
 }
 
 function getAddGame(req, res) {
 
-    res.render("add-game", {title: "Add Game"})
+    res.render("pages/add-game", {title: "Add Game"})
 }
 
 
