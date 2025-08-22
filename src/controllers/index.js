f13d090cbbe39d740f4b3194d7c92a678a7552cf
bilal <bilalghazal98@gmail.com>
2025-08-22 15:17:14 +0300

    Set up data and needed library
    Set up the data in the browse genres page and use Slugify library to format text
    for use in urls and set up the correcting formatting for urls in <a> tags

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,4 +1,5 @@
 const db = require("../db/queries")
+const slugify = require("slugify")
 
 
 async function getHomepage(req, res) {
@@ -14,8 +15,14 @@ function getAddGame(req, res) {
 
 async function getGenres(req, res) {
     const data = await db.getGenresFromDatabase()
+
+    const dataMod = data.map(datum => ({
+        ...datum,
+        urlName: slugify(datum.name, {lower: true})
+    }))
+
     
-    res.render("pages/browse-genres", {title: "Browse Genres", data: data})
+    res.render("pages/browse-genres", {title: "Browse Genres", data: dataMod})
 }
 
 
