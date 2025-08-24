4a6246c5f67a977709107b7064fba7777a98793b
bilal <bilalghazal98@gmail.com>
2025-08-24 15:38:55 +0300

    Change the name of the controller of index to index-controller and a controller for games

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const indexController = require("../controllers/index")
+const indexController = require("../controllers/index-controller")
 const gameController = require("../controllers/game-controller")
 
 router.get("/", indexController.getHomepage)
@@ -10,5 +10,6 @@ router.get("/add-game", indexController.getAddGame)
 router.get("/browse-genres", indexController.getGenres)
 
 router.get("/game/:id", gameController.getIndividualGamePage)
+
 
 module.exports = router
