ddb3484d5decaa6591eba0f2b32441cc28dea8dc
bilal <bilalghazal98@gmail.com>
2025-08-25 15:28:34 +0300

    Refactor the function for getting genres in the index controller
    Refactor the function so that the id of the genre can be obtained from the URL and use to search for games with that genre

--- src/controllers/index-controller.js
+++ src/controllers/index-controller.js
@@ -21,7 +21,7 @@ async function getGenres(req, res) {
 
     const dataMod = data.map(datum => ({
         ...datum,
-        urlName: slugify(datum.name, {lower: true})
+        urlName: slugify(`${datum.name} ${datum.id}`, {lower: true})
     }))
 
     
