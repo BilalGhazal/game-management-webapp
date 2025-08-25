fe80477764ed63b769edc4287d6ca2a7c7248703
bilal <bilalghazal98@gmail.com>
2025-08-25 15:17:42 +0300

    Finish styling individual game page and genres page
    Finish the styling of the individual game page and change the styling of the genres
    page so that the container of the genres fills the body container of the main layout 100%

--- src/controllers/game-controller.js
+++ src/controllers/game-controller.js
@@ -3,11 +3,7 @@ const db = require("../db/queries")
 
 async function getIndividualGamePage(req, res) {
     const id = req.params.id
-    const data = await db.getIndividualGameInfo(id)
-
-    console.log(id)
-    console.log(data)
-    
+    const data = await db.getIndividualGameInfo(id)   
 
     if (data) {
         res.render("pages/individual-game-page", {title: data.title, data: data})
