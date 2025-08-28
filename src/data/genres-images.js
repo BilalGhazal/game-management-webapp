0e1fbfb1cebe07bc47b8e98034a513af5bf64fef
bilal <bilalghazal98@gmail.com>
2025-08-28 21:04:48 +0300

    Populate the genres images object with data

---
+++ src/data/genres-images.js
@@ -0,0 +1,25 @@
+const genresImages = {
+    "action": "https://media.rawg.io/media/games/4a0/4a0a1316102366260e6f38fd2a9cfdce.jpg",
+    "adventure": "https://media.rawg.io/media/games/21c/21cc15d233117c6809ec86870559e105.jpg",
+    "rpg": "https://media.rawg.io/media/games/530/5302dd22a190e664531236ca724e8726.jpg",
+    "shooter": "https://media.rawg.io/media/games/f87/f87457e8347484033cb34cde6101d08d.jpg",
+    "strategy": "https://media.rawg.io/media/games/d1e/d1e70ce3762efcfc170c6bd067d7e9e3.jpg",
+    "simulation": "https://media.rawg.io/media/games/dd5/dd50d4266915d56dd5b63ae1bf72606a.jpg",
+    "sports": "https://media.rawg.io/media/games/0c1/0c1c9965ba59166ab986a663ab2252dc.jpg",
+    "racing": "https://media.rawg.io/media/games/7cf/7cf38d2d80267c121c6d0d361e9429ce.jpg",
+    "puzzle": "https://media.rawg.io/media/games/7fa/7fa0b586293c5861ee32490e953a4996.jpg",
+    "platformer": "https://media.rawg.io/media/games/718/71891d2484a592d871e91dc826707e1c.jpg",
+    "fighting": "https://media.rawg.io/media/games/684/684ecc08397479de72c5f89ef6f16f4f.jpg",
+    "horror": "https://media.rawg.io/media/resize/420/-/games/daa/daaee07fcb40744d90cf8142f94a241f.jpg",
+    "stealth": "https://media.rawg.io/media/resize/420/-/games/d46/d46373f39458670305704ef089387520.jpg",
+    "survival": "https://media.rawg.io/media/resize/420/-/games/58a/58ac7f6569259dcc0b60b921869b19fc.jpg",
+    "sandbox / open world": "https://media.rawg.io/media/games/37a/37a9536e92cf8fe3b60045aa75dbd41f.jpg",
+    "rhythm / music": "https://media.rawg.io/media/games/e4f/e4fb3fd188f61fabec48dca22e6ef28a.jpg",
+    "party / casual": "https://media.rawg.io/media/games/48c/48cb04ca483be865e3a83119c94e6097.jpg",
+    "moba": "https://media.rawg.io/media/resize/420/-/games/1bd/1bd2657b81eb0c99338120ad444b24ff.jpg",
+    "mmorpg": "https://media.rawg.io/media/games/82b/82be203e68d737762846203811165933.jpg",
+    "card / board": "https://media.rawg.io/media/screenshots/8ff/8ffe8f19d2e764867c8ed625ddf4e368.jpg",
+    "other": "https://media.rawg.io/media/games/3af/3af386b6e26be6741b711ae6215ef42f.jpg" 
+}
+
+module.exports = {genresImages}
