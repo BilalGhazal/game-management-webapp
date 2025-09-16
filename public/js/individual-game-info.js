09a0aa805f8afd75cf2861ce94ae61215c11e593
bilal <bilalghazal98@gmail.com>
2025-09-17 00:18:10 +0300

    Refactor template code for individual game page and add the deletion process with warning of incorrect password and closing dialog when done and redirecting to homepage

--- public/js/individual-game-info.js
+++ public/js/individual-game-info.js
@@ -4,6 +4,7 @@ const closeDialog = document.querySelector(".dialog-close-button")
 const form = document.querySelector("#deleteGameForm")
 const passwordInput = document.querySelector("#password")
 const confirmDeleteButton = document.querySelector(".dialog-delete-button")
+const errorMassageSpan = document.querySelector(".error-message")
 
 
 const path = window.location.pathname
@@ -39,20 +40,46 @@ closeDialog.addEventListener("click", () => {
 confirmDeleteButton.addEventListener("click", async(event) => {
     event.preventDefault()
 
+    if (!form.reportValidity()) return
+
     const formData = new FormData(form)
 
     const password = formData.get("password")
 
-    const response = await fetch("/api/delete/game/checkpassword", {
+    try {
+        const response = await fetch("/api/delete/game/checkpassword", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ password })
-    })
+        })
 
-    if (response.ok) {
-        alert("Password correct!")
+        if (response.ok) {
+            errorMassageSpan.textContent = ""
+            const response = await fetch(`/api/delete/game/${gameId}`, {method: "DELETE"})
+
+            if (response.ok) {
+                dialog.close(), 500
+                window.location.href = "/"
+            }
+
+            else {
+                alert("Something went wrong")
+            }
+
+        }
+
+        else if (response.status === 401) {
+            errorMassageSpan.textContent = "Incorrect password"
+            form.reset()   
+        }
+
+        else {
+            alert("Something went wrong")
+        }
     }
-    else {
-        alert("Wrong password")
+
+    catch(err) {
+        console.error("Network error: ", err)
     }
 })
+    
--- src/controllers/game-controller.js
+++ src/controllers/game-controller.js
@@ -93,7 +93,7 @@ async function deleteGameFromDatabase(req, res) {
 
     try {
         await db.deleteGameInfo(id)
-        res.status(200).end()
+        res.send(200).end()
     }
 
     catch (err) {
